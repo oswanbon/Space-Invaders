177b186d4a6e683ccdaf510f9b7f2fbce6e68ee1
oswanbon <33901673+oswanbon@users.noreply.github.com>
2017-12-05 05:25:51 -0800

    space invaders through part 2

---
+++ spaceinvaders.py
@@ -0,0 +1,59 @@
+#Part 3: Game Object, Border, Boundary Checking
+import os
+import random
+import turtle
+
+turtle.fd(0)
+turtle.speed(0)
+turtle.bgcolor("black")
+turtle.ht()
+turtle.setundobuffer(1)
+turtle.tracer(1)
+
+class Sprite(turtle.Turtle):
+    def __init__(self, spriteshape, color, startx, starty):
+        turtle.Turtle.__init__(self, shape = spriteshape)
+        self.speed(0)
+        self.penup()
+        self.color(color)
+        self.fd(0)
+        self.goto(startx,starty)
+        self.speed = 1
+
+    def move(self):
+        self.fd(self.speed)
+
+
+
+class Player(Sprite):
+    def __init__(self, spriteshape, color, startx, starty):
+        Sprite.__init__(self, spriteshape, color, startx, starty)
+        self.speed = 4
+        self.lives = 3
+
+    def turn_left(self):
+        self.lt(45)
+
+    def turn_right(self):
+        self.lt(45)
+
+    def accelerate(self):
+        self.speed += 1
+
+    def decelerate(self):
+        self.speed -= 1
+
+
+#Create my sprites
+player = Player("triangle", "white", 0,0)
+
+#keyboard bindings
+turtle.listen()
+turtle.onkey(player.turn_left, "Left")
+turtle.onkey(player.turn_right, "Right")
+turtle.onkey(player.accelerate, "Up")
+turtle.onkey(player.decelerate, "Down")
+
+#Game Loop
+while True:
+    player.move()
