19dace3dba1bbd278a3db70a12780dbab0dc8bc2
oswanbon <33901673+oswanbon@users.noreply.github.com>
2017-12-07 05:36:10 -0800

    Space Invaders through part 6

--- spaceinvaders.py
+++ spaceinvaders.py
@@ -1,4 +1,4 @@
-#Part 4: Create an Enemy/ Collision Checking
+#Part 6: Create an Ally
 import os
 import random
 import turtle
@@ -75,6 +75,64 @@ class Enemy(Sprite):
         self.speed = 6
         self.setheading(random.randint(0,360))
 
+class Ally(Sprite):
+    def __init__(self, spriteshape, color, startx, starty):
+        Sprite.__init__(self,spriteshape,color,startx,starty)
+        self.speed = 8
+        self.setheading(random.randint(0,360))
+
+    def move(self):
+        self.fd(self.speed)
+
+        # Boundary detection
+        if self.xcor() > 290:
+            self.setx(290)
+            self.lt(60)
+        #
+        if self.xcor() < -290:
+            self.setx(-290)
+            self.lt(60)
+        #
+        if self.ycor() > 290:
+            self.sety(290)
+            self.lt(60)
+        #
+        if self.ycor() < -290:
+            self.sety(-290)
+            self.lt(60)
+
+class Missile(Sprite):
+    def __init__(self, spriteshape, color, startx, starty):
+        Sprite.__init__(self,spriteshape, color, startx, starty)
+        self.shapesize(stretch_wid=0.3, stretch_len=0.4, outline=None)
+        self.speed = 20
+        self.status = "ready"
+        self.goto(-1000,1000)
+
+    def fire(self):
+        if self.status == "ready":
+            self.goto(player.xcor(), player.ycor())
+            self.setheading(player.heading())
+            self.status = "firing"
+
+    def move(self):
+        if self.status == "ready":
+            self.goto(-1000,1000)
+
+        if self.status == "firing":
+            self.fd(self.speed)
+
+        if self.status == "firing":
+            self.fd(self.speed)
+
+        #Border check
+        #How can we add this to our Sprite?
+        if self.xcor() < -290 or self.xcor() > 290 or \
+                self.ycor() < -290 or self.ycor() > 290:
+            self.goto(-1000,1000)
+            self.status = "ready"
+
+
 
 class Game():
     def __init__(self):
@@ -107,6 +165,9 @@ game.draw_border()
 #Create my sprites
 player = Player("triangle", "white", 0,0)
 enemy = Enemy("circle", "red", -100,0)
+missile = Missile("triangle", "yellow", 0,0)
+ally = Ally("square", "blue", 0,0)
+
 
 #keyboard bindings
 turtle.listen()
@@ -114,14 +175,30 @@ turtle.onkey(player.turn_left, "Left")
 turtle.onkey(player.turn_right, "Right")
 turtle.onkey(player.accelerate, "Up")
 turtle.onkey(player.decelerate, "Down")
+turtle.onkey(missile.fire, "space")
 
 #Game Loop
 while True:
     player.move()
     enemy.move()
+    missile.move()
+    ally.move()
 
-    #Check for a collision
+    #Check for a collision with the player
     if player.is_collision(enemy):
         x = random.randint(-250, 250)
         y = random.randint(-250, 250)
         enemy.goto(x, y)
+
+    #Check for a collision between the missile and the enemy
+    if missile.is_collision(enemy):
+        x = random.randint(-250, 250)
+        y = random.randint(-250, 250)
+        enemy.goto(x, y)
+        missile.status = "ready"
+
+    if missile.is_collision(ally):
+        x = random.randint(-250, 250)
+        y = random.randint(-250, 250)
+        ally.goto(x, y)
+        missile.status = "ready"
